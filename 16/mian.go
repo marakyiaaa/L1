73b7aa4abc6de967572c53a35d2d01f1ab8845d7
teodorad <teodorad@student.21-school.ru>
2024-12-08 20:41:43 +0300

    exp 16

---
+++ 16/mian.go
@@ -0,0 +1,20 @@
+package main
+
+func partition(arr []int, low, high int) []int
+
+func partition(arr []int, low, high int) ([]int, int) {
+	pivot := arr[high]
+	i := low
+	for j := low; j < high; j++ {
+		if arr[j] < pivot {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		}
+	}
+	arr[i], arr[high] = arr[high], arr[i]
+	return arr, i
+}
+
+func main() {
+
+}
